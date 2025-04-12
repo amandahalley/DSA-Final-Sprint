52ee77611c2c05814bb25d243f73113377a80c60
ahllyy <amanda.halley@keyin.com>
2025-04-12 11:18:01 -0230

    adding routes and structure for main applicaiton

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import CreateTreePage from './pages/CreateTreePage';
+import PreviousTreesPage from './pages/PreviousTreesPage';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <div style={{ padding: '20px', fontFamily: 'Arial' }}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/create" element={<CreateTreePage />} />
+          <Route path="/history" element={<PreviousTreesPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
