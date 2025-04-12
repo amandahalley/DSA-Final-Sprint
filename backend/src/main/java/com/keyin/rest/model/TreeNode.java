924b4e2d00302119f8a1aa3e3ef8cbbe399700eb
ahllyy <amanda.halley@keyin.com>
2025-04-12 11:08:39 -0230

    implemented logic for creating tree structure

--- backend/src/main/java/com/keyin/rest/model/TreeNode.java
+++ backend/src/main/java/com/keyin/rest/model/TreeNode.java
@@ -1,4 +1,12 @@
 package com.keyin.rest.model;
 
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TreeNode {
+    private int value;
+    private TreeNode left;
+    private TreeNode right;
 }
