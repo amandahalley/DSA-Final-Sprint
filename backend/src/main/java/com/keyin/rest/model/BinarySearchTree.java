69abb8bed3040ff206945650111de657217b6434
ahllyy <amanda.halley@keyin.com>
2025-04-12 11:12:57 -0230

    implemented binary search tree logic for numbers

--- backend/src/main/java/com/keyin/rest/model/BinarySearchTree.java
+++ backend/src/main/java/com/keyin/rest/model/BinarySearchTree.java
@@ -1,4 +1,23 @@
 package com.keyin.rest.model;
 
+
+import lombok.Data;
+
+@Data
 public class BinarySearchTree {
+    private TreeNode root;
+
+    public void insert(int value) {
+        root = insertRecursion(root, value);
+    }
+
+    private TreeNode insertRecursion(TreeNode node, int value) {
+        if (node == null) return new TreeNode(value, null, null);
+        if (value < node.getValue())
+            node.setLeft(insertRecursion(node.getLeft(), value));
+        else
+            node.setRight(insertRecursion(node.getRight(),value));
+        return node;
+    }
+
 }
